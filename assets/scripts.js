7a8ffc15e798ac3c2ab579afefa338808b74f15a
sohaibb2 <sohaibb@outlook.com>
2022-11-30 20:18:14 -0600

    lab 8

--- assets/scripts.js
+++ assets/scripts.js
@@ -7,17 +7,30 @@ function addToCart(cookie) {
         sandies: 30
         party press: 35
         chocolate chip: 25
-    */.
+    */
    
    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
 
+   itemCount = itemCount + 1
+   console.log(itemCount)
    //add 1 to the itemCount variable
    //add the correct price to the currentPrice variable
+   if (cookie == "peanut butter") {
+    currentPrice = currentPrice + 20
+} else if (cookie == "sandies") {
+    currentPrice = currentPrice + 30
+} else if ( cookie == "party press") {
+    currentPrice = currentPrice + 35
+} else if (cookie = "chocolate chip") {
+    currentPrice = currentPrice + 25
+}
 
+
+   console.log(currentPrice)
 }
 
 function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
-    
+    window.alert(`Item Count: ${itemCount} Total Cost: ${currentPrice}`)
 }
