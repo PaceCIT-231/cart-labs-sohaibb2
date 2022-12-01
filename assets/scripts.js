09cfa1920b936676802191ae3615361682b650ae
sohaibb2 <sohaibb@outlook.com>
2022-11-30 20:34:36 -0600

    lab 9 .

--- assets/scripts.js
+++ assets/scripts.js
@@ -7,17 +7,53 @@ function addToCart(cookie) {
         sandies: 30
         party press: 35
         chocolate chip: 25
-    */.
+    */
+   
    
    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
 
-   //add 1 to the itemCount variable
-   //add the correct price to the currentPrice variable
+   itemCount = itemCount + 1
+   console.log(itemCount)
+   
 
+   document.getElementById("cartItems").innerHTML= itemCount
+
+   if (cookie == "peanut butter") {
+    currentPrice = currentPrice + 20
+} else if (cookie == "sandies") {
+    currentPrice = currentPrice + 30
+} else if ( cookie == "party press") {
+    currentPrice = currentPrice + 35
+} else if (cookie = "chocolate chip") {
+    currentPrice = currentPrice + 25
+}
+
+document.querySelector(".hoverText").innerHTML = currentPrice
+
+
+   console.log(currentPrice)
 }
 
 function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
-    
+
+    prompt(`What is your Name and Address so we can ship your items to you?`)
+    currentPrice = 0
+    itemCount = 0
+
+    document.getElementById("cartItems").innerHTML= itemCount
+    document.querySelector(".hoverText").innerHTML = currentPrice
+
 }
+
+function darkMode() {
+    document.querySelector("body").style.color = "white"
+    document.querySelector("body").style.backgroundColor = "black"
+
+} 
+
+
+
+
+
