468f43940c723bfe54af0db7ad220608b160322d
sohaibb2 <sohaibb@outlook.com>
2022-12-08 01:08:42 -0600

    push

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,29 @@
-let currentPrice=0, itemCount=0
+const cart = {
+    currentPrice: 0,
+    items: [],
+    
+    addItem: function(cookie, price) {
+        // add a cookie (string) to the items array
+    this.items.push(cookie)
+        //add the price (number) to the currentPrice properties
+    this.currentPrice= this.currentPrice + price 
+    },
+
+    clear: function() {
+        //reset the currentPrice and items properties
+        this.currentPrice= 0
+        this.items= []
+    },
+
+    getSummary: function() {
+        let summary = `<p>Number of Items: ${this.items.length}</p>
+            <h4>Details</h4>
+            <p>${this.items.join('<br>')}</p>
+            <p>Total Price: $${this.currentPrice}</p>`
+        return summary
+    },
+}
+
 
 function addToCart(cookie) {
     /* 
@@ -7,22 +32,47 @@ function addToCart(cookie) {
         sandies: 30
         party press: 35
         chocolate chip: 25
-    */.
+    */
    
    console.log('The user is adding this type of cookie to their cart: ' , cookie) 
 
+
+
    //add 1 to the itemCount variable
    //add the correct price to the currentPrice variable
+   if (cookie == 'peanut butter'){cart.addItem(cookie, 20)}
+   if (cookie == 'sandies') {cart.addItem(cookie, 30)}
+   if (cookie == 'party press') {cart.addItem(cookie, 35)}
+   if (cookie == 'chocolate chip') {cart.addItem(cookie, 25)}
 
+   document.querySelector(".hoverText").innerHTML = cart.currentPrice
+    console.log(cart)
+    document.getElementById("cartItems").innerHTML= cart.items.length
 }
 
-function addPriceToProduct (product, price) {
-    product.price = price
-    return product.price
-    }
+
 
 function checkout() {
     console.log('User is checking out.')
     //Let your customer know how many items they are purchasing and the price
-    
+    window.alert(`Item Count: ${cart.items.length} Total Cost: ${cart.currentPrice}`)
+    prompt(`What is your name and address so we can bill you and send you your purchases?`)
+    cart.clear()
+    document.querySelector(".hoverText").innerHTML = cart.currentPrice
+    console.log(cart)
+    document.getElementById("cartItems").innerHTML= cart.items.length
+}
+
+
+function darkMode() {
+	document.querySelector("body").style.backgroundColor="navy"
+    document.querySelector("body").style.color="white"
+}
+
+
+function clearCart() {
+    cart.clear()
+    document.querySelector(".hoverText").innerHTML = cart.currentPrice
+    console.log(cart)
+    document.getElementById("cartItems").innerHTML= cart.items.length
 }
